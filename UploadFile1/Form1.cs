0747c8ed9f38e10bf533d067f38ee8e853665a16
arjun patil <arjunatil839@gmail.com>
2020-06-28 10:53:44 +0530

    Add project files.

---
+++ UploadFile1/Form1.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace UploadFile1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
